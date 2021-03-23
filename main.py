813ea84a990d35ca9afb6c0401674eee352e7071
RockoonTechnologies <66290320+RockoonTechnologies@users.noreply.github.com>
2021-03-22 21:14:17 -0400

    e3e21

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, flash, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
 from Users.User import *
 import Users
 app = Flask(__name__)
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = "static/CDN"
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/signup', methods=["POST"])
 def signup():
-   try:
+    try:
         username = request.args.get("username")
         password = request.args.get("password")
         email = request.args.get("email")
@@ -31,6 +41,19 @@ def login():
 def index():
     return render_template("index.html")
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+            return jsonify({"Error": "No files were provided"})
+    file = request.files['file']
+
+    if file.filename == '':
+            return jsonify({"Error": "No files were provided, or it is empty"})
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({"Result": "Success"})
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
